04c2c8490101288f7943690fec3a04177167e1ea
Alan Woodward <alan.woodward@romseysoftware.co.uk>
2016-10-07 14:29:33 +0100

    Swap to navigation pills rather than tabs

--- src/main/resources/assets/application.js
+++ src/main/resources/assets/application.js
@@ -5,6 +5,10 @@ var fieldsData = [
 ];
 
 var NavBar = ReactBootstrap.Navbar;
+var Nav = ReactBootstrap.Nav;
+var NavItem = ReactBootstrap.NavItem;
+var Col = ReactBootstrap.Col;
+
 var Tabs = ReactBootstrap.Tabs;
 var Tab = ReactBootstrap.Tab;
 
@@ -38,31 +42,47 @@ var Segments = React.createClass({
     render: function() {
         var segmenttab = this.props.segments.map(function(f, i) {
             var name = "Segment " + f.ord;
-            return (<Tab eventKey={i + 1} title={name}>Segment {f.ord}</Tab>);
+            return (<NavItem eventKey={i + 1}>{name}</NavItem>);
         });
-        segmenttab.unshift(<Tab eventKey={0} title="All segments">All segments</Tab>);
-        return (<Tabs position="left">{segmenttab}</Tabs>);
+        segmenttab.unshift(<NavItem eventKey={0}>All segments</NavItem>);
+        return (<Nav bsStyle="pills" stacked onSelect={this.props.onSelect}>{segmenttab}</Nav>)
     }
 });
 
 var MarpleContent = React.createClass({
     getInitialState: function() {
-        return { indexpath: "loading", generation: -1, segments: []}
+        return {
+            indexData: { indexpath: "loading", generation: -1, segments: []},
+            view: (<Col md={10}>Select a segment</Col>)
+        }
     },
     componentDidMount: function() {
         $.ajax({
             url: "http://localhost:8080/api/index",
             dataType: 'json',
             success: function(data) {
-                this.setState(data);
+                var newState = this.state;
+                newState.indexData = data;
+                this.setState(newState);
             }.bind(this)
         });
     },
+    selectSegment: function(segNumber) {
+        this.state.view = (
+            <Fields segment={segNumber}/>
+        );
+        this.setState(this.state);
+    },
     render: function() {
-        return (<div>
-                    <MarpleNav indexData={this.state}/>
-                    <Segments segments={this.state.segments}/>
-                </div>)
+        return (
+            <div>
+                <MarpleNav indexData={this.state.indexData}/>
+                <Col md={2}>
+                    <Segments segments={this.state.indexData.segments} onSelect={this.selectSegment}/>
+                </Col>
+                {this.state.view}
+            </div>
+        )
     }
 });
 
