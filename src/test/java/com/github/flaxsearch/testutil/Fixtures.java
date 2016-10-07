b64b3d9ce88c49e0270cb49036d91761b6ba4384
Alan Woodward <alan.woodward@romseysoftware.co.uk>
2016-10-07 11:01:41 +0100

    Add simple run script, and write Fixture index to disk

--- src/test/java/com/github/flaxsearch/testutil/Fixtures.java
+++ src/test/java/com/github/flaxsearch/testutil/Fixtures.java
@@ -17,6 +17,8 @@ package com.github.flaxsearch.testutil;
  */
 
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.Field;
@@ -25,6 +27,7 @@ import org.apache.lucene.document.TextField;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.store.RAMDirectory;
 
 public class Fixtures {
@@ -33,6 +36,13 @@ public class Fixtures {
         RAMDirectory directory = new RAMDirectory();
         populateIndex(directory);
         return directory;
+    }
+
+    public static void main(String... args) throws IOException {
+        Path index = Paths.get(args[0]);
+        try (Directory directory = FSDirectory.open(index)) {
+            populateIndex(directory);
+        }
     }
 
     private static void populateIndex(Directory directory) {
