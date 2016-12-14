477e9103bd7af898b32f55a611f5e7a17dd0f592
Tom Mortimer <tom@flax.co.uk>
2016-12-14 17:04:15 +0000

    Started to add some shallow render tests

--- ui/test/components/terms-test.js
+++ ui/test/components/terms-test.js
@@ -1,7 +1,11 @@
 import expect from 'expect.js';
+import React from 'react';
+import { renderShallow } from '../test-helper';
+import Terms from '../../src/components/terms';
+
 
 describe('components/terms', () => {
-  it('works', () => {
+  it('FIXME', () => {
     expect(1).to.eql(1);
   });
 });
